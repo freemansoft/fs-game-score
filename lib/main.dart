935ab41d24ed99e7af9b4fdff1e4b97b8fa8a40c
Joe Freeman <joe@freemansoft.com>
2025-12-23 20:19:55 -0500

    iOS 26.1 on ipads related to double tap window behavior https://github.com/flutter/flutter/issues/175606#issuecomment-3453392532

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
 import 'package:fs_score_card/app.dart';
 
+// https://github.com/flutter/flutter/issues/175606#issuecomment-3453392532
+// Error introduced with IOS 26.1
+class FilteringFlutterBinding extends WidgetsFlutterBinding {
+  @override
+  void handlePointerEvent(PointerEvent event) {
+    if (event.position == Offset.zero) {
+      return;
+    }
+    super.handlePointerEvent(event);
+  }
+}
+
 void main() {
+  FilteringFlutterBinding();
   if (kIsWeb) {
     WidgetsFlutterBinding.ensureInitialized();
     SemanticsBinding.instance.ensureSemantics();
